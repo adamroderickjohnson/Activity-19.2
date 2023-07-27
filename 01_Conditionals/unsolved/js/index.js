eacdba404664775aed0b0751393beed032a206c9
adamroderickjohnson <adam.roderick.johnson@gmail.com>
2023-07-27 18:58:24 -0400

    Cmon commit already

--- 01_Conditionals/unsolved/js/index.js
+++ 01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,13 @@
 $('.tab').on("click", function(){
-  
+  if($(this).hasClass("tab1")){
+    $(".tab1").addClass("active");
+    $("#tabContent1").css("display", "block");
+    $(".tab2").removeClass("active");
+    $("#tabContent2").css("display", "none");
+  }else if($(this).hasClass("tab2")){
+    $(".tab2").addClass("active");
+    $("#tabContent2").css("display", "block");
+    $(".tab1").removeClass("active");
+    $("#tabContent1").css("display", "none");
+  }
 });
